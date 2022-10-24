3449c8b4a7919f8c47a5cfae12a92b6a49fa2f62
elisaveta <115875206+elitry@users.noreply.github.com>
2022-10-24 13:44:55 +0300

    Лабораторная работа 2

---
+++ task8.py
@@ -0,0 +1,10 @@
+money_capital = 10000
+salary = 5000
+spend = 6000
+increase = 0.05
+month = 0  # количество месяцев, которое можно прожить
+while money_capital > spend:
+    month += 1
+    spend = spend + spend * increase
+    money_capital = (money_capital - spend) + salary
+print(month)
